981b6d4e72d838dbbaf9da68e9d9df0f46f4176c
InLearningPhase <taneshgupta2211@gmail.com>
2020-01-15 16:12:42 +0530

    add parameter

--- src/adt/atm.java
+++ src/adt/atm.java
@@ -1,8 +1,8 @@
 package adt;
 
 public interface atm {
-    public abstract double deposit();
-    public abstract double withdraw();
+    public abstract double deposit(double amount);
+    public abstract double withdraw(double amount);
     public abstract void showBalance();
-    public abstract boolean verifyPin();
+    public abstract boolean verifyPin(int atmPin);
 }
